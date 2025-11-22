b471b4fdf914e2c57d6144761d434bd2d2b46c54
705mw <maciej.maciuk11@gmail.com>
2025-11-22 19:52:43 +0100

    Further cleaning, startgame doesn't wait on preparedeck for some reason xD

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 
-
-var available_dice = 6;
 var scanners = 0;
 var hp = 8;
 var shields = 4;
 var deck = [];
-var active_threats = []
+var active_threats = [];
+var available_crew_dice = 6;
+var crew_dice = [];
+
 
 
 async function loadCards() {
   var response = await fetch('cards.json');
   deck = await response.json();
   console.log(deck);
-  return(deck);
 }
 
-function prepareDeck(gameDifficulty, gameLength){
+async function prepareDeck(gameDifficulty, gameLength){
     switch(gameDifficulty){
         case("EASY"):
             deck.splice(-6, 1)
@@ -34,7 +34,9 @@ function prepareDeck(gameDifficulty, gameLength){
     const j = Math.floor(Math.random() * (i + 1)); 
     [deck[i], deck[j]] = [deck[j], deck[i]]; 
     }
+
     console.log(deck);
+
     switch(gameLength){
         case("SHORTENED"):
             deck.splice(-5,5);
@@ -44,21 +46,19 @@ function prepareDeck(gameDifficulty, gameLength){
             break;
     }
     console.log(deck);
-    return(deck)
 }
 
-function rtd(dice){
-    let arr = [];
-    for(let i = 0; i<dice;i++){
-        arr.push(Math.floor((Math.random() * 6)+1));
+function rollCrew(){
+    crew_dice = [];
+    for(let i = 0; i<available_crew_dice;i++){
+        crew_dice.push(Math.floor((Math.random() * 6)+1));
     }
-    console.log(arr);
-    return arr;
+    console.log(crew_dice);
 }
 
 
 function lockIn(){
-
+    
 }
 
 function assignCrew(){
@@ -68,31 +68,35 @@ function assignCrew(){
 function resolveThreats(){
     active_threats.forEach(threat => {
         if(threat.type = "standard"){
-            console.log("special")
+            console.log("standard")
         }else if(threat.type = "special"){
-            console.log("Standard") 
+            console.log("special") 
         }
     });
 }
 
 
 function gatherCrew(){
-    
+
 }
 
 
 async function startGame() {
   await loadCards();  
-  prepareDeck("HARD", "SHORTENED");  
-  while(deck.length>0){
+  await prepareDeck("EASY", "SHORT");  
+  active_threats.push(deck[4]);
+  console.log(active_threats);
+  resolveThreats();
+  /*while(deck.length>0){
     //kroki po kolei wdg instrukcji dsd6
-    rtd();
+    rollCrew();
     lockIn();
     assignCrew();
     drawThreat();
     resolveThreats();
     gatherCrew();
-  }
+    
+  }*/
 }
 
 startGame();
